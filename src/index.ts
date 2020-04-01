e6a684d2d70b3f3229d63efad32d0b4c21104509
Jenifer <jenifer.teles.n@gmail.com>
2020-04-01 15:17:49 -0300

    Novas planilhas
    Commit das novas planilhas, relatório: 02/04/2020

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,44 @@
+import fs from "fs"
 
+
+(async ()=>{
+
+    let caminho = `C:\\Users\\Arvvo\\WebstormProjects\\Scripts\\Excel\\Planilhas\\Relatorios\\Relatorios\\Participantes`
+    function pastaToCSV(pathPasta) {
+        // Ler o nome de todos os arquivos dentro da pasta ( csv )
+        let arquivos = fs.readdirSync(pathPasta)
+
+        function leArquivo(nome){
+            let caminhoDoArquivo = pathPasta + "\\" + nome
+            return fs.readFileSync(caminhoDoArquivo, "latin1" )
+        }
+
+        let conteudos = arquivos.map(leArquivo)
+
+        function removerCabecalho(conteudoCsv) {
+            let linhas = conteudoCsv.split("\n")
+            return linhas.splice(1).join("\n")
+        }
+
+        let conteudosSemCabecalho = conteudos.map(removerCabecalho)
+
+        let cabecalho = conteudos[0].split("\n")[0]
+
+        let resultado = cabecalho + "\n" + conteudosSemCabecalho.join("\n")
+
+        return resultado
+
+    }
+
+
+    let conteudoCsvGigante = pastaToCSV(caminho)
+
+    let novocaminho = "C:\\Users\\Arvvo\\WebstormProjects\\Scripts\\Excel\\Planilhas\\Relatorios\\Relatorios\\Participantes"
+
+    fs.writeFileSync(novocaminho + "\\" + "Total_Participantes", conteudoCsvGigante)
+
+
+})();
 
 
 
